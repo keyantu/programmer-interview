1a03036f7519fcd66432e47c5771bd09d8894cd7
keyantu <infiniet93@gmail.com>
2015-05-13 15:01:19 +0800

    3.1第六章练习
    赋值运算符左边必须是变量，否则编译报错

--- code_test/test_603.cpp
+++ code_test/test_603.cpp
@@ -229,3 +229,16 @@ int main(){
 	return 0;
 }
 */
+//≤π≥Ã–Ú‘±√Ê ‘±¶µ‰p44-6.1.1
+//±‡“Î±®¥Ì
+/*
+#define SUB(x,y) x-y
+#define ACCESS_BEFORE(element,offset,value) *SUB(&element, offset)=value 
+int main(){
+	int i;
+	int array[10] = {1,2,3,4,5,6,7,8,9,10};
+	ACCESS_BEFORE(array[5],4,6);
+	cout<<"happy"<<endl;
+	return 0;
+}
+*/
