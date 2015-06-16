5384f87300dfd0a256bbe1f128c9e6c620c41c53
keyantu <infiniet93@gmail.com>
2015-06-16 16:20:56 +0800

    chp14 update

--- code_test/test_1404-6.cpp
+++ code_test/test_1404-6.cpp
@@ -115,3 +115,219 @@ pair<string, int> func2(string str)
 */
 //------------------------------------------------------------------------------------------------
 //------14.05-----面试例题3
+//返回主字符串中字符字串位置以后所有的字符
+/*
+#include <iostream>
+using namespace std;
+const char* strstr1(const char* str, const char* strCharSet){
+	for (int i=0;str[i]!='\0';i++)
+	{
+		int j = 0;
+		int temp = i;
+		if (str[i] == strCharSet[j])
+		{
+			while (str[i++] == strCharSet[j++])
+			{
+				if ((strCharSet[j]=='\0'))
+				{
+					return &str[i-j];
+				}
+				i = temp;
+			}
+		}
+	}
+	return NULL;
+}
+*/
+//------------------------------------------------------------------------------------------------
+//------14.05-----面试例题4
+//一句话中单词倒置
+/*
+#include <iostream>
+
+using namespace std;
+
+char *strReverse(char * sourcestr)
+{
+	int j = 0, i = 0,begin,end;
+	char *str = sourcestr;
+	char temp;
+	j = strlen(str) - 1;
+	cout << " string = " << str << endl;
+
+	while (j > i)
+	{
+		temp = str[i];
+		str[i] = str[j];
+		str[j] = temp;
+		j --;
+		i ++;
+	}
+	cout << " string = " << str << endl;
+	
+
+	i = 0;
+	while (str[i])
+	{
+		if (str[i] != ' ')
+		{
+			begin = i;
+			while (str[i] && str[i] != ' ')
+			{
+				end = i;
+				i++;
+			}
+			if (str[i] == '\0') //???????
+			{
+				i--;
+			}
+		}
+		while (end > begin)
+		{
+			temp = str[begin];
+			str[begin] = str[end];
+			str[end] = temp;
+			end --;
+			begin ++;
+		}
+		i ++;
+	}
+	cout << " string = " << str << endl;
+	return str;
+}
+
+int main()
+{
+	char str[] = "I come from Tianjin.";
+	strReverse(str);
+	return 0;
+}
+*/
+//------------------------------------------------------------------------------------------------
+//------14.05-----面试例题5
+/*
+#include <iostream>
+using namespace std;
+typedef int Type;
+Type Count1InAInteger(Type n)
+{
+	Type iNum=0;
+	while(n!=0)
+	{
+		iNum+=(n%10==1)?1:0;
+		n/=10;
+	}
+	return iNum;
+}
+Type f(Type n)
+{
+	Type iCount=0;
+	for(Type i=1;i<=n;i++)
+	{
+		iCount+=Count1InAInteger(i);
+	}
+	return iCount;
+}
+Type Sum(Type n)
+{
+	Type Count = 0;
+	Type Factor = 1; 
+	Type LowerNum = 0;
+	Type CurrNum = 0;
+	Type HigherNum = 0;
+	while(n/Factor != 0)
+	{
+		LowerNum = n - (n / Factor) * Factor;
+		CurrNum = (n / Factor)%10;
+		HigherNum = n /(Factor *10);
+		switch(CurrNum)
+		{
+		case 0:
+			Count += HigherNum * Factor;
+			break;
+		case 1:
+			Count += HigherNum * Factor + LowerNum +1 ;
+			break;
+		default:
+			Count += (HigherNum + 1) * Factor;
+			break;
+		}
+		Factor *= 10;
+	}
+	return Count;
+}
+int main()
+{
+	Type num;
+	cin>> num;
+	
+	cout << Sum(num) << endl;	
+	cout << f(num) << endl;
+	return 1;
+}
+*/
+//------------------------------------------------------------------------------------------------
+//------14.06-----面试例题1
+/************************************************************************
+转换字符串格式为原来字符串里的字符+该字符连续出现的个数，
+例如：1233422222 转换为1121324125（1出现1次，2出现1次，3出现2次...... 
+*************************************************************************/ 
+/*
+#include<iostream>   
+#include<cstring>  
+#include<cstdlib>  
+using namespace std;  
+void convert(char input[],char output[])  
+{  
+	
+    int count = 1;  
+    char *fast = input;  
+    char *slow = input;  
+	
+    while(*(++fast)!='\0')  
+    {  
+        if(*fast == *slow)//如果当前读入的字符和正在统计的一样，那么出现次数要++   
+        {  
+            count++;  
+            continue;  
+        }  
+        // 当快慢指针不同时就执行下边语句  
+        if(slow<fast)  //注意不能写成while 形式 因为要求重复的数字只出现一次  
+        { 
+            *output++=*slow++;   
+        } 
+        *output++=count+'0';  //将出现次数添加至数字后边  
+        slow = fast;  //  慢指针指向快指针 重新统计  
+        count = 1;  //计数器重置为初始值  
+    }  
+    *output++=*slow;  // 快指针跳出循环时慢指针仍没有跳出循环但是count计数正确  
+    *output++=count+'0';//因为读到'\0'循环退出，最后一个连续的字符序列没有输出，所以在这里追加输出   
+    *output='\0'; 
+}  
+int main() 
+{ 
+    char input[]="1233422222"; 
+    char output[30]={0}; 
+    convert(input,output); 
+    cout<<"output is "<<output<<endl;
+	return 0;
+} 
+*/
+//------------------------------------------------------------------------------------------------
+//------14.06-----面试例题2
+/*
+void fun(char* w,int m){
+	int i =0;
+	int len = strlen(w);
+	if(m>len) m = len;
+
+	while (len-m>0||(m=0)!=0)
+	{
+		for (i=0,w[len]=w[0],++m;i<len;i++)
+		{
+			w[i] = w[i+1];
+		}
+		w[len-m] = '\0';
+	}
+}
+*/
